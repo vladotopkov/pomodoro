8e900fdbdb80316ae5f5f27b78ba175697db3de4
bot <hello@bot.com>
2025-05-31 12:35:59 +0300

    working version of pomodoro

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ let cyclesNumberInput = document.getElementById('cycles');
 let isWorkPhase = false;
 let isBreakPhase = false;
 let stopped = true;
-let paused = true;
+let isPaused = false;
 let cycleCount = 0;
 let cycles = 0;
 
@@ -36,7 +36,7 @@ function playBeep() {
 //countdown timer logic
 
 
-let startMinutes = 0.02;   
+let startMinutes = 6;   
 let time = startMinutes * 60;
 let countdown;
 
@@ -95,8 +95,8 @@ function changeTime(duration) {
 }
 
 
-function startCountLogic(){
-    updateTimer(); 
+function startCountLogic(startImmediately = true){
+    if (startImmediately) updateTimer();
     countdown = setInterval(updateTimer, 1000);
 }
 
@@ -105,9 +105,9 @@ function startCountLogic(){
 
 //event listeners
 
-
 //start of cycle
 startBtn.addEventListener('click', (e) => {
+    startBtn.disabled = true;
     console.log(workCount);
     if(isWorkPhase || isBreakPhase) return;
     isWorkPhase = true;
@@ -118,6 +118,7 @@ startBtn.addEventListener('click', (e) => {
 
 //reset cycles
 stopBtn.addEventListener('click', (e) => {
+    startBtn.disabled = false;
     clearInterval(countdown);
     timerDisplay.innerText = "25:00";
     isWorkPhase  = false;
@@ -127,8 +128,14 @@ stopBtn.addEventListener('click', (e) => {
 
 
 //pause
-pauseBtn.addEventListener('click', (e) => {
-    
+pauseBtn.addEventListener('click', () => {
+    if (!isPaused) {
+        clearInterval(countdown);
+        isPaused = true;
+    } else {
+        startCountLogic(false); // resume without immediate tick
+        isPaused = false;
+    }
 });
 
 
